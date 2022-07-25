7c25618bfcd75edcdde1ddcd0fac83c98f32368a
Aleksandr <a_bogdanov@inbox.ru>
2022-07-25 16:12:06 +0300

    описан метод, отбирающий строки с количеством символов менее или равно 3

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string []somewords = new string [7] {"шла", "саша", "по", "шоссе", "и", "сосала", "сушку"};
+string []shortwords = new string [somewords.Length];
+
+void ThreeLetters (string[]somewords, string[] shortwords)
+{
+    int count = 0;
+    for (int i = 0; i < somewords.Length; i++)
+    {
+        if(somewords[i].Length <= 3)
+        {
+            shortwords[count] = somewords[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+ThreeLetters (somewords, shortwords);
+PrintArray (shortwords);
